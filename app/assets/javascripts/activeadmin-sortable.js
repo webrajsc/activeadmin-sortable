4a2a6c125e82bfcf5348b6f782d4d569c69d3e02
Alexander Panasyuk <panasmeister@gmail.com>
2015-03-17 12:29:15 +0700

    Even and Odd rows operations

--- app/assets/javascripts/activeadmin-sortable.js
+++ app/assets/javascripts/activeadmin-sortable.js
@@ -7,12 +7,21 @@
     this.sortable({
       update: function(event, ui) {
         var url = ui.item.find('[data-sort-url]').data('sort-url');
-
         $.ajax({
           url: url,
           type: 'post',
           data: { position: ui.item.index() + 1 },
-          success: function() { window.location.reload() }
+          success: function(e) {
+            $('.handle').closest('tbody').find('tr').each(
+                function(i, tr){
+                    var element = $(tr);
+                    if(element.hasClass('even') || element.hasClass('odd')) {
+                        element.removeClass('even odd');
+                        element.addClass(i % 2 ? 'even' : 'odd');
+                    }
+                }
+            )
+          }
         });
       }
     });
